8f1aba9d922925db3ab0921bac3327e79b8882a7
Quoc Toan Vu <quoctoanvu@Quocs-MacBook-Air.local>
2018-05-28 00:44:12 +0700

    1st

---
+++ src/TestComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class TestComp extends Component {
+  render() {
+    return (
+      <div>
+        <label>CSS module</label>
+        <button>Button</button>
+      </div>
+    );
+  }
+}
